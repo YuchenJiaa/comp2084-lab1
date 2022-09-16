8910441ea596e372bc77af3f357cbd3a360cf6ce
YuchenJiaa <yuchenjiacc@gmail.com>
2022-09-16 16:41:47 -0400

    Add project files.

---
+++ comp2084-lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comp2084_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
